cdd886ade9854fde1a1b6232158af59a0987ced2
Thomas Ryan Stovall <trstovall@gmail.com>
2019-01-24 02:10:25 -0600

    specialize for bbe251

--- galois.py
+++ galois.py
@@ -1,45 +1,100 @@
 
-'''
-Field inversion for BBE251 elliptic curve
-'''
+sc_modulus = 2**249 + 17672450755679567125975931502191870417
+fe_modulus = 2**251 + 2**7 + 2**4 + 2**2 + 1
+d1 = 2**57 + d**54 + d**44 + 1
 
-def mul(a, b, m):
+def fe_mul(a, b):
     c = 0
     while b:
         if b & 1:
             c ^= a
         a <<= 1
         b >>= 1
-    if c.bit_length() < m.bit_length():
+    if c < 2**251:
         return c
-    n = m.bit_length() - 1
-    return (c & ((1 << n) - 1)) ^ mul(c >> n, m & ((1 << n) - 1), m)
+    return (c % 2**251) ^ fe_mul(c >> 251, 2**7 + 2**4 + 2**2 + 1)
 
 
-def exp(x, n, m):
+def fe_sq(a):
+    return fe_mul(a, a)
+
+
+d1d1 = fe_mul(d1, d1)
+
+
+def fe_exp(x, n):
     xx = 1
     while n:
         if n & 1:
-            xx = mul(xx, x, m)
-        x = mul(x, x, m)
+            xx = fe_mul(xx, x)
+        x = fe_mul(x, x)
         n >>= 1
     return xx
 
 
-def inv(x, m):
-    return exp(x, 2**(m.bit_length() - 1) - 2, m)
+def fe_sqrt(x):
+    # pth root of x = x ^ (p ^ (m - 1)), for x in G(2^m)
+    return fe_exp(x, 2**250)
 
 
-def params_1():
-    m = 2**8 + 2**4 + 2**3 + 2**1 + 1
-    a = 2**8 + 2**0
-    b = 2**7 + 2**6 + 2**5 + 2**4 + 2**3 + 2**2 + 2**0
-    return a, b, m
+sqrtd1 = fe_sqrt(d1)
 
 
-def params_bbe251():
-    m = 2**251 + 2**7 + 2**4 + 2**2 + 1
-    d = 2**57 + 2**54 + 2**44 + 1
-    return m, d
+def fe_inv(x):
+    return fe_exp(x, 2**251 - 2)
 
+
+def fe_rand():
+    return fe_mul(sum(x << (8*i) for i, x in enumerate(urandom(32))), 1)
+
+
+def trace(x):
+    pass
+
+
+def htrace(x):
+    pass
+
+
+def ge_add(P, Q):
+    # https://hyperelliptic.org/EFD/g12o/auto-edwards-xyz-1.html
+    # add-2008-blr-3
+    (X1, Y1, Z1) = P
+    (X2, Y2, Z2) = Q
+    A = fe_mul(X1, X2)
+    B = fe_mul(Y1, Y2)
+    C = fe_mul(Z1, Z2)
+    D = fe_mul(d1, C)
+    E = fe_mul(C, C)
+    F = fe_mul(d1d1, E)
+    G = fe_mul(X1 ^ Z1, X2 ^ Z2)
+    H = fe_mul(Y1 ^ Z1, Y2 ^ Z2)
+    I = A ^ G
+    J = B ^ H
+    K = fe_mul(X1 ^ Y1, X2 ^ Y2)
+    L = fe_mul(d1, K)
+    U = fe_mul(C, F ^ fe_mul(L, K ^ I ^ J ^ C))
+    V = U ^ fe_mul(D, F) ^ fe_mul(
+        L, fe_mul(d1, E) ^ fe_mul(G, H) ^ fe_mul(A, B)
+    )
+    X3 = V ^ fe_mul(fe_mul(D, A ^ D), G ^ D)
+    Y3 = V ^ fe_mul(fe_mul(D, B ^ D), H ^ D)
+    Z3 = U
+    return (X3, Y3, Z3)
+
+
+def ge_dbl(P):
+    # https://hyperelliptic.org/EFD/g12o/auto-edwards-xyz-1.html
+    # dbl-2008-blr-2
+    (X1, Y1, Z1) = P
+    W1 = X1 ^ Y1
+    E = fe_sq(fe_mul(W1, W1 ^ Z1))
+    X3 = fe_sq(
+        fe_mul(fe_mul(sqrtd1, W1) ^ X1, Z1) ^ fe_sq(X1)
+    )
+    Y3 = X3 ^ E
+    Z3 = E ^ fe_mul(d1, fe_sq(fe_sq(Z1)))
+    return (X3, Y3, Z3)
+
+    
 
