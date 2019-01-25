916bfa33ba8b5c30a9a8afd1599187e5faaed717
trstovall <trstovall@users.noreply.github.com>
2019-01-25 01:24:25 -0600

    add test for crypto.trace

--- crypto_test.py
+++ crypto_test.py
@@ -37,3 +37,28 @@ def test_trace_bits():
         a = [reduce(i) for i in sq(a)]
     bits = reduce(sum(x, []))
     assert bits == trace_bits
+
+
+def test_trace():
+    from crypto import m, reduction_poly, trace, fe_rand
+    def sq(x):
+        a = 0
+        for i in range(m):
+            if x & (2**i):
+                a |= 2**(2*i)
+        for i in range(2):
+            x, a = a >> m, a % (2**m)
+            for bit in reduction_poly:
+                a ^= x << bit
+        return a
+    def tr(x):
+        a = 0
+        for i in range(m):
+            a ^= x
+            x = sq(x)
+        x = 0
+        for i in range(m):
+            x ^= a % 2
+            a >> 1
+        return x
+    assert all(tr(x) == trace(x) for x in (fe_rand() for i in range(100)))
